215b0726b2b89cf5b586c98c4b4ef46c9d85695d
VICK <84926820+VICK-sys@users.noreply.github.com>
2026-09-16 13:00:02 -0400

    [BUGFIX] Fixed Error in Credits

--- lib/https.lua
+++ lib/https.lua
@@ -4,22 +4,25 @@ local OS, https = love.system.getOS(), "https"
 
 if OS == "Windows" then
 	if love.filesystem.getInfo("lib/windows/https.dll", "file") then
-		https = "lib/windows/https"
+		https = "lib/windows/https.dll"
 	end
 elseif OS == "Linux" then
 	if love.filesystem.getInfo("lib/linux/https.so", "file") then
-		https = "lib/linux/https"
+		https = "lib/linux/https.so"
 	end
 elseif OS == "OS X" then
 	if love.filesystem.getInfo("lib/osx/https.so", "file") then
-		https = "lib/osx/https"
+		https = "lib/osx/https.so"
 	end
 end
 
 local success, v = pcall(package.loadlib, https, "luaopen_https")
-if success then
-	return v
-else
-	local __NULL__ = function() end
-	return setmetatable({}, {__index = function() return __NULL__ end})
+if success and type(v) == "function" then
+	success, v = pcall(v, "https")
+	if success and type(v) == "table" and type(v.request) == "function" then
+		return v
+	end
 end
+
+local __NULL__ = function() end
+return setmetatable({}, {__index = function() return __NULL__ end})
